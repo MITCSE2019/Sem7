1ac3113a7effdfe206fcc1942d5dfffcc4e26767
Prajjwal Rai <prajjwalrai.11@gmail.com>
2018-09-12 11:30:00 +0530

    Add files via upload

---
+++ Week_6/p3/main.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="main.aspx.cs" Inherits="main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+        </asp:DropDownList>
+        <br />
+        <br />
+        TextBox&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week_6/p3/main.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class main : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            DropDownList1.Items.Add(new ListItem("Summer", "Summer"));
+            DropDownList1.Items.Add(new ListItem("Monsoon", "Monsoon"));
+            if(Session["Theme"] != null )
+            {
+                DropDownList1.Items.FindByText(Session["Theme"].ToString()).Selected = true;
+            }
+        }
+    }
+    protected void Page_PreInit(object sender, EventArgs e )
+    {
+        if(Session["Theme"] != null )
+        {
+            Page.Theme = Session["Theme"].ToString();
+        }
+    }
+    protected void DropDown1_SelectedIndexChanged( object sender, EventArgs e )
+    {
+        Session["Theme"] = DropDownList1.SelectedValue.ToString();
+        Server.Transfer(Request.FilePath);
+    }
+
+    /*protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session["Theme"] = DropDownList1.SelectedValue;
+        Server.Transfer(Request.FilePath);
+    }*/
+}
