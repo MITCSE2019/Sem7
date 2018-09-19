e020cbb08079af01456ace5d883c3d49035d9011
Prajjwal Rai <prajjwalrai.11@gmail.com>
2018-09-19 11:31:07 +0530

    Add files via upload

---
+++ Week_8/p3/update.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="update.aspx.cs" Inherits="update" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Value&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Update" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week_8/p3/update.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class update : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = @"Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=Products;Integrated Security=True;Pooling=False";
+        con.Open();
+        string id = TextBox1.Text;
+        string price = TextBox2.Text;
+        int i, j;
+        int.TryParse(id, out i);
+        int.TryParse(price, out j);
+        string sql = "Update Items set price = " + i + "where itemID = " + j;
+        SqlCommand cmd = new SqlCommand(sql, con);
+        cmd.ExecuteNonQuery();
+        //Label1.Text = a.ToString();
+        cmd.CommandType = System.Data.CommandType.Text;
+
+        /*using (SqlDataReader dr = cmd.ExecuteReader())
+        {
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+        }*/
+
+        con.Close();
+    }
+}
