e47482d82c6b6d0ff6970e67e5653b716b3675dd
Prajjwal Rai <prajjwalrai.11@gmail.com>
2018-09-19 10:53:51 +0530

    Add files via upload

---
+++ Week_8/p4/first.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="first.aspx.cs" Inherits="first" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Company:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Salary:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Street:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        City:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+        <br />
+        <br />
+        <asp:Label ID="Result" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        Enter company( for employee search ):&nbsp; <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" Text="Search" OnClick="Button2_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week_8/p4/first.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class first : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            //SqlConnection con = new SqlConnection();
+            //con.ConnectionString = @"Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=employee;Integrated Security=True;Pooling=False";
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = @"Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=employee;Integrated Security=True;Pooling=False";
+        string name, comp, street, city;
+        int sal;
+        name = TextBox1.Text;
+        comp = TextBox2.Text;
+        int.TryParse(TextBox3.Text, out sal);
+        street = TextBox4.Text;
+        city = TextBox5.Text;
+        var insertCommand = "INSERT INTO works (person_name, Company, Salary) Values(@1, @2, @3)";
+        var insertCommand2 = "INSERT INTO lives (person_name, Street, City) Values(@6, @4, @5)";
+        SqlCommand command = new SqlCommand(insertCommand, con);
+        SqlCommand command2 = new SqlCommand(insertCommand2, con);
+        command.Parameters.AddWithValue("@1", name);
+        command.Parameters.AddWithValue("@2", comp);
+        command.Parameters.AddWithValue("@3", sal);
+        command2.Parameters.AddWithValue("@4", street);
+        command2.Parameters.AddWithValue("@5", city);
+        command2.Parameters.AddWithValue("@6", name);
+        try
+        {
+            con.Open();
+            int added1,added2;
+            added1 = command.ExecuteNonQuery();
+            added2 = command2.ExecuteNonQuery();
+            Result.Text = added1.ToString() + " records inserted in works and ." + added2.ToString() + "records inserted in lives.";
+            //var insertCommand = "INSERT INTO works (person_name, Company, Salary) Values(@1, @2, @3)";
+            //SqlCommand cmd = new SqlCommand(insertCommand, con);
+        }
+        catch (Exception ex )
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = @"Data Source=(localdb)\MSSQLlocalDB;Initial Catalog=employee;Integrated Security=True;Pooling=False";
+        con.Open();
+        string comp = TextBox6.Text;
+        string sql = "Select lives.person_name,City from lives join works on works.person_name=lives.person_name where Company='" + comp + "'";
+        SqlCommand cmd = new SqlCommand(sql, con);
+        cmd.CommandType = System.Data.CommandType.Text;
+
+        using (SqlDataReader dr = cmd.ExecuteReader())
+        {
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+        }
+
+        con.Close();
+    }
+}
