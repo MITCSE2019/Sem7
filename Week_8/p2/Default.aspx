e27052c20574c93913b8f3616695ccac3abdcef3
Prajjwal Rai <prajjwalrai.11@gmail.com>
2018-09-26 11:11:07 +0530

    Contributed By Nayak

---
+++ Week_8/p2/Default.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Add data"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="p_name" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="company"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="p_company" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Salary"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="salary" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label10" runat="server" Text="street"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="p_street" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="p_city" runat="server" Text="city"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="pcity" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="res1" runat="server" Text="Result="></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Search"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label9" runat="server" Text="company"></asp:Label>
+&nbsp;
+        <asp:DropDownList ID="s_company" runat="server">
+        </asp:DropDownList>
+        <br />
+&nbsp;&nbsp;
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="res2" runat="server" Text="Search result : "></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Search" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week_8/p2/Default.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source = (localdb)\\MSSQLlocalDB;Initial Catalog=week7_4;Integrated Security=True;Pooling=False;MultipleActiveResultSets=true";
+
+            try
+            {
+                con.Open();
+                string q = "SELECT distinct company_name FROM WORKS";
+                SqlCommand query = new SqlCommand(q, con);
+                query = new SqlCommand(q, con);
+                SqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    s_company.Items.Add(new ListItem(reader["company_name"].ToString(), reader["company_name"].ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                Button1.Text = ex.ToString();
+            }
+            finally
+            {
+                //close
+                con.Close();
+            }
+        }
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Data Source = (localdb)\\MSSQLlocalDB;Initial Catalog=week7_4;Integrated Security=True;Pooling=False;MultipleActiveResultSets=true";
+        
+        try
+        {
+            //open
+            con.Open();
+
+
+
+            string q = "INSERT INTO WORKS VALUES ('" + p_name.Text + "','" + p_company.Text + "','" + salary.Text + "')";
+            SqlCommand query = new SqlCommand(q,con);
+            query.ExecuteNonQuery();
+
+            q = "INSERT INTO LIVES VALUES ('" + p_name.Text + "','" + p_street.Text + "','" + pcity.Text + "')";
+            query = new SqlCommand(q, con);
+            query.ExecuteNonQuery();
+
+            q = "SELECT * FROM WORKS WHERE person_name='"+p_name.Text+"'";
+            query = new SqlCommand(q, con);
+            SqlDataReader reader1 = query.ExecuteReader();
+
+            
+            q = "SELECT * FROM LIVES WHERE person_name='" + p_name.Text + "'";
+            query = new SqlCommand(q, con);
+            SqlDataReader reader2 = query.ExecuteReader();
+
+
+            if (reader1.HasRows && reader2.HasRows)
+            {
+                res1.Text+= "Success";
+            }
+            else
+            {
+                res1.Text+= "Not added";
+            }
+        }
+        catch (Exception ex)
+        {
+            Button1.Text = ex.ToString();            
+        }
+        finally
+        {
+            //close
+            con.Close();
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Data Source = (localdb)\\MSSQLlocalDB;Initial Catalog=week7_4;Integrated Security=True;Pooling=False;MultipleActiveResultSets=true";
+
+        try
+        {
+            //open
+
+            con.Open();
+
+            string q = "SELECT * FROM WORKS as W,LIVES as L WHERE W.person_name=L.person_name AND W.company_name = '"+s_company.Items[s_company.SelectedIndex].Value+"'";
+            SqlCommand query = new SqlCommand(q, con);          
+            query = new SqlCommand(q, con);
+            SqlDataReader reader = query.ExecuteReader();
+            res2.Text = "";
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    res2.Text+=reader["person_name"].ToString() + reader["company_name"].ToString() + reader["salary"].ToString() + "</br>";
+                }
+            }
+            else
+            {
+                res2.Text = "No result";
+            }
+        }
+        catch (Exception ex)
+        {
+            Button1.Text = ex.ToString();
+        }
+        finally
+        {
+            //close
+            con.Close();
+        }
+    }
+}
